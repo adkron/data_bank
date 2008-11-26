5c1fbdb2f180d8cea672223a3242f97bdbfe3dba
Amos King <amos.l.king@gmail.com>
2008-11-25 20:34:04 -0600

    added rdoc and changed the withdraw method to not print anything

--- lib/data_bank.rb
+++ lib/data_bank.rb
@@ -1,6 +1,18 @@
+# This gem eases data marshaling from one run of a script
+# to the next.
+#
+# Author::    Amos King  (mailto:amos.l.king@gmail.com)
+# Copyright:: Copyright (c) 2008 Amos King
+# License::   Released under the MIT license
+
 require 'base64'
 
 class DataBank
+  # Stores a base64 encoded representation of a class at
+  # the end of the file it is called from.  Also adds the
+  # __END__ directive to let the interperter know not to
+  # go beyond that point.  On subsequent calls it will
+  # replace the data with the new data passed in.
   def self.deposit(data)
     code = File.read($0).gsub(/\n^__END__.*$/m, '')
     File.open($0, 'w') do |file|
@@ -9,11 +21,9 @@ class DataBank
     end
   end
   
+  # Loads the data stored at the end of the file form
+  # which it is called.  If data is present.
   def self.withdraw
-    begin
-      Marshal.load(Base64.decode64(DATA.read))
-    rescue NameError
-      puts 'can not reload data'
-    end
+    Marshal.load(Base64.decode64(DATA.read)) rescue NameError
   end
 end
