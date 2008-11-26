abd47be378e68feab676e935c40b3d5f3fb9e99f
Amos King <amos.l.king@gmail.com>
2008-11-25 22:36:53 -0600

    add things to be picked up by rdoc for the example file

--- example.rb
+++ example.rb
@@ -1,3 +1,19 @@
+# require 'rubygems'
+# require 'data_bank'
+# 
+# class MyObject
+#   attr_accessor :data
+# 
+#   def initialize(data)
+#     @data = data
+#   end
+# end
+# 
+# my_object = DataBank.withdraw || MyObject.new(1)
+# my_object.data += 1
+# puts my_object.data
+# DataBank.deposit my_object
+
 require 'rubygems'
 require 'data_bank'
 
